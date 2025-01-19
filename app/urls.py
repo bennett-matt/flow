c790fd56007929594f1066c8d7f597df6ed806ef
Matthew Bennett <matthew@bennett-web.net>
2025-01-18 17:07:42 -0700

    updating for frontend

--- app/urls.py
+++ app/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('flow/', include('flow.urls')),
+    path('', include('flow.urls')),
     path('admin/', admin.site.urls),
 ]
--- flow/views.py
+++ flow/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the flow index.")
+    return render(request, 'home.html')
+
+def session(request):
+  return render(request, 'sessions/session.html')
